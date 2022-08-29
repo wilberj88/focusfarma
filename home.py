1ef5f33d02560bd6222cbfca51bbb44863b29d28
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-29 12:02:21 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Energy", page_icon=":battery:")
+
+st.title('Focus Farma 🔋')
+st.header("Enfócate, con un poco de apoyo")
+
+st.write("Bienvenido a la botánica personalizada exclusivamente para ti")
+
+st.markdown(
+  """
+  En esta web encontrarás:
+  - 🔎 _    Diagnóstico para enfocarte en el trabajo, el sueño y la fiesta
+  - 🛒 _    Receta personalizada a tus Necesidades
+  - ✍️ _     Propuesta Inteligente de Suministro
+    
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
