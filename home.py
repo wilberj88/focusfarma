2ea3580a3d13fc4a36ff12c7e68bbad8d37726ba
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-29 12:15:55 -0500

    Update home.py

--- home.py
+++ home.py
@@ -3,19 +3,19 @@ import pandas as pd
 import numpy as np
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="Novus Energy", page_icon=":battery:")
+st.set_page_config(layout="wide", page_title="Focus Farma", page_icon="🎯 ")
 
-st.title('Focus Farma 🔋')
-st.header("Enfócate, con un poco de apoyo")
+st.title('Focus Farma 🍃🎯🍃')
+st.header("Enfócate, con nuestro apoyo")
 
-st.write("Bienvenido a la botánica personalizada exclusivamente para ti")
+st.write("Bienvenido a la botánica personalizada para ti")
 
 st.markdown(
   """
   En esta web encontrarás:
-  - 🔎 _    Diagnóstico para enfocarte en el trabajo, el sueño y la fiesta
-  - 🛒 _    Receta personalizada a tus Necesidades
-  - ✍️ _     Propuesta Inteligente de Suministro
+  - 🔎 _   Diagnóstico para enfocarte en el trabajo, el sueño y la fiesta
+  - 🛒 _   Receta personalizada a tus Necesidades
+  - ✍️ _    Propuesta Inteligente de Suministro
     
   EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
   """
